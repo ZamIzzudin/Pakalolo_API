d100f1093d40d99db10670f0d70588b977280946
ZamIzzudin <azzamizzudinhasan@gmail.com>
2022-06-08 15:53:59 +0700

    change database site

--- config/index.js
+++ config/index.js
@@ -10,9 +10,9 @@ const mysql = require('mysql');
 
 const conn = mysql.createConnection({
     host: 'sql6.freemysqlhosting.net',
-    user: 'sql6495309',
-    password: 'TvtD7EgtRF',
-    database: 'sql6495309',
+    user: 'sql6498424',
+    password: 'i6CavtqFW9',
+    database: 'sql6498424',
     port: 3306
 })
 
