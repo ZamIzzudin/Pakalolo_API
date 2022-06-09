7efbba72152fddfb48dc23aa51897f5f9bddbc7e
ZamIzzudin <azzamizzudinhasan@gmail.com>
2022-06-09 08:33:11 +0700

    fix cors setting

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ const app = express();
 
 const router = require('./routes');
 
-app.use(cors({
-    origin: 'https://pakalolo.netlify.app/'
-}));
+app.use(cors());
 
 app.use(helmet());
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
